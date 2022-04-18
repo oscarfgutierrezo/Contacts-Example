7f552a5a945a630c2ae929631564fe23a4558533
Oscar Gutierrez <oscarfgutierrezo@gmail.com>
2022-04-18 13:36:19 -0500

    Add: contacts-01 to contacts-05 sections

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        "dark-violet": "#1E0E62",
+        "dark-violet-2": "#151439",
+        "light-violet": "#482BE7",
+        "green": "#25DAC5",
+        "primary": "#2F1893",
+        "red": "#EA3223",
+        "pink": "#E93A7D",
+        "blue": "#1DA1F2",
+        "grey": "#EBEAED",
+      },
+      fontFamily: {
+        "sans": "DM Sans"
+      },
+      backgroundImage: {
+        cont01: 'url("../img/contacts-01.png")',
+        cont02: 'url("../img/contacts-02.png")',
+        cont04: 'url("../img/contacts-04.png")',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
